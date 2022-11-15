98dae1d76934363c9bc05f1e3c45a5ca6a9b01dd
Giovanni Spadaccini <giospadaccini74@gmail.com>
2022-11-15 19:28:50 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use eframe::egui;
+
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let painter=ui.painter();
+            use egui::epaint::{CircleShape};
+            use egui::Color32;
+            let shape=CircleShape::filled(egui::Pos2 { x: 1., y: 1. },1.0, Color32::from_rgb(1,2,3));
+            painter.add(shape);
+        });
+    }
+}
