521658e7b3d487ee7ed80b303f93356c52099c36
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2022-12-03 21:16:25 +0100

    feat(grammar): reduce unitary cycles

--- src/grammar/grammar.rs
+++ src/grammar/grammar.rs
@@ -1,11 +1,11 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, BTreeMap};
 
 use crate::automata::dfa::DFA;
 
 pub type NonTerminal = usize;
 pub type Terminal = char;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, PartialOrd, Eq, Ord)]
 pub enum Letter {
     NonTerminal(NonTerminal),
     Terminal(Terminal),
@@ -289,10 +289,103 @@ impl Grammar {
                 }
             })
         });
+
+        // invalidate nullable
+        self.nullable = None;
     }
 
-    pub fn remove_cycles(&mut self) {
-        unimplemented!("remove_cycles");
+    pub fn get_unitary_couples(&self) -> BTreeSet<(NonTerminal, NonTerminal)>  {
+        let non_terminals = self.get_non_terminal();
+        let mut unitary_couples = BTreeSet::new();
+        let mut has_changed = true;
+
+        for non_terminal in non_terminals {
+            unitary_couples.insert((non_terminal, non_terminal));
+        }
+        
+        while has_changed {
+            has_changed = false;
+            for production in self.productions.iter() {
+                if production.rhs.len() != 1 {
+                    continue;
+                }
+                let mut to_insert = BTreeSet::new();
+                for unitary_couple in unitary_couples.iter() {
+                    if let Letter::NonTerminal(non_term) = production.rhs[0] {
+                        if unitary_couple.1 == production.lhs && 
+                         !unitary_couples.contains(&(unitary_couple.0, non_term)) &&
+                         !to_insert.contains(&(unitary_couple.0, non_term)) {
+                            to_insert.insert((unitary_couple.0, non_term));
+                        }
+                    }
+                }
+
+                if to_insert.len() > 0 {
+                    unitary_couples.append(&mut to_insert);
+                    has_changed = true;
+                }
+            }
+        }
+
+        unitary_couples
+    }
+
+    // TODO: this is a very complex function in this moment, it needs refactor
+    // it also has some points were it can be optimized 
+    pub fn remove_unitary_cycles(&mut self) {
+        let unitary_couples = self.get_unitary_couples();
+
+        // remove all unitary couples
+        self.productions.retain(|production| {
+            if production.rhs.len() != 1 {
+                return true;
+            }
+
+            match production.rhs[0] {
+                Letter::NonTerminal(non_term) => !unitary_couples.contains(&(production.lhs, non_term)),
+                Letter::Terminal(_) => true
+            }
+        });
+
+        // add corresponding productions 
+        let mut adj_list = self.transitions_to_adj_list();
+        for unitary_couple in unitary_couples.iter() {
+            if unitary_couple.0 == unitary_couple.1 {
+                continue;
+            }
+
+            let mut to_insert = adj_list.get(&unitary_couple.1).unwrap().clone();
+
+            adj_list.entry(unitary_couple.0)
+                .or_insert(BTreeSet::new())
+                .append(&mut to_insert);
+        }
+
+        // trasform adj list back to transitions
+        let mut new_transitions = vec![];
+        for (non_terminal, transitions) in adj_list.iter() {
+            for transition in transitions.iter() {
+                new_transitions.push(Production {
+                    lhs: *non_terminal,
+                    rhs: transition.clone()
+                });
+            }
+        }
+        self.productions = new_transitions;
+
+        // invalidate nullable
+        self.nullable = None;
+    }
+
+    fn transitions_to_adj_list(&self) -> BTreeMap<NonTerminal, BTreeSet<Vec<Letter>>> {
+        let mut adj_list: BTreeMap<NonTerminal, BTreeSet<Vec<Letter>>> = BTreeMap::new();
+        for production in self.productions.iter() {
+            adj_list.entry(production.lhs)
+                .or_insert(BTreeSet::new())
+                .insert(production.rhs.clone());
+        }
+
+        adj_list
     }
 }
 
@@ -457,6 +550,49 @@ mod test {
             ],
             nullable: None,
         };
+
+        assert_eq!(grammar, result);
+    }
+
+    #[test]
+    fn test_remove_unitary_cycles() {
+        // E -> E + T | T
+        // T -> T * F | F
+        // F -> (E) | a 
+        
+        let mut grammar = Grammar {
+            start_symbol: 0,
+            productions: vec![
+                Production { lhs: 0, rhs: vec![Letter::NonTerminal(0), Letter::Terminal('+'), Letter::NonTerminal(1)] },
+                Production { lhs: 0, rhs: vec![Letter::NonTerminal(1)] },
+                Production { lhs: 1, rhs: vec![Letter::NonTerminal(1), Letter::Terminal('*'), Letter::NonTerminal(2)] },
+                Production { lhs: 1, rhs: vec![Letter::NonTerminal(2)] },
+                Production { lhs: 2, rhs: vec![Letter::Terminal('('), Letter::NonTerminal(0), Letter::Terminal(')')] },
+                Production { lhs: 2, rhs: vec![Letter::Terminal('a')] },
+            ],
+            nullable: None,
+        };
+
+        let result = Grammar {
+            // E -> E + T | T * F | (E) | a
+            // T -> T * F | (E) | a
+            // F -> (E) | a
+            start_symbol: 0,
+            productions: vec![
+                Production { lhs: 0, rhs: vec![Letter::NonTerminal(0), Letter::Terminal('+'), Letter::NonTerminal(1)] },
+                Production { lhs: 0, rhs: vec![Letter::NonTerminal(1), Letter::Terminal('*'), Letter::NonTerminal(2)] },
+                Production { lhs: 0, rhs: vec![Letter::Terminal('('), Letter::NonTerminal(0), Letter::Terminal(')')] },
+                Production { lhs: 0, rhs: vec![Letter::Terminal('a')] },
+                Production { lhs: 1, rhs: vec![Letter::NonTerminal(1), Letter::Terminal('*'), Letter::NonTerminal(2)] },
+                Production { lhs: 1, rhs: vec![Letter::Terminal('('), Letter::NonTerminal(0), Letter::Terminal(')')] },
+                Production { lhs: 1, rhs: vec![Letter::Terminal('a')] },
+                Production { lhs: 2, rhs: vec![Letter::Terminal('('), Letter::NonTerminal(0), Letter::Terminal(')')] },
+                Production { lhs: 2, rhs: vec![Letter::Terminal('a')] },
+            ],
+            nullable: None,
+        };
+
+        grammar.remove_unitary_cycles();
 
         assert_eq!(grammar, result);
     }
