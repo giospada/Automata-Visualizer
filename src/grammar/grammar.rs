1cd02c7689d8c0562df511fb6bdef28bcc96f548
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2022-12-03 16:44:21 +0100

    feat(grammar): add remove useless non terminals

--- src/grammar/grammar.rs
+++ src/grammar/grammar.rs
@@ -209,8 +209,86 @@ impl Grammar {
         nullable
     }
 
-    pub fn remove_useless(&mut self) {
-        unimplemented!("remove_useless");
+    /// O(m^2) implementation of reachable function, could be optimized
+    /// but i need to store adjacency list of the graph in grammar, and the
+    /// use bfs.
+    pub fn get_reachable(&self) -> BTreeSet<usize> {
+        let mut reachable = BTreeSet::new();
+        let mut has_changed = true;
+        reachable.insert(self.start_symbol);
+        while has_changed {
+            has_changed = false;
+            for production in self.productions.iter() {
+                if !reachable.contains(&production.lhs) {
+                    continue;
+                }
+                for letter in production.rhs.iter() {
+                    match letter {
+                        Letter::NonTerminal(idx) => {
+                            if !reachable.contains(idx) {
+                                reachable.insert(*idx);
+                                has_changed = true;
+                            }
+                        }
+                        Letter::Terminal(_) => {}
+                    }
+                }
+            }
+        }
+
+        reachable
+    }
+
+    /// returns set of generator non terminals
+    /// a non terminal is a generator when it produces some finite
+    /// string of terminals
+    pub fn get_generators(&self) -> BTreeSet<usize> {
+        let mut generators = BTreeSet::new();
+        for production in self.productions.iter() {
+            let mut is_generator = true;
+            for letter in production.rhs.iter() {
+                match letter {
+                    Letter::NonTerminal(non_terminal) => {
+                        if !generators.contains(non_terminal) {
+                            is_generator = false;
+                            break;
+                        }
+                    }
+                    Letter::Terminal(_) => {}
+                }
+            }
+            if is_generator {
+                generators.insert(production.lhs);
+            }
+        }
+
+        generators
+    }
+
+    pub fn remove_useless(&mut self) -> () {
+        // first remove non generators
+        let generators = self.get_generators();
+
+        self.productions.retain(|production| {
+            generators.contains(&production.lhs) && production.rhs.iter().all(|letter| {
+                match letter {
+                    Letter::NonTerminal(idx) => generators.contains(idx),
+                    Letter::Terminal(_) => true
+                }
+            })
+        });
+
+        // then remove non reachable
+        let reachable = self.get_reachable();
+
+        self.productions.retain(|production| {
+            reachable.contains(&production.lhs) && production.rhs.iter().all(|letter| {
+                match letter {
+                    Letter::NonTerminal(idx) => reachable.contains(idx),
+                    Letter::Terminal(_) => true
+                }
+            })
+        });
     }
 
     pub fn remove_cycles(&mut self) {
@@ -342,6 +420,40 @@ mod test {
                 Production { lhs: 2, rhs: vec![Letter::Terminal('a'), Letter::NonTerminal(2)] },
                 Production { lhs: 2, rhs: vec![Letter::Terminal('b'), Letter::NonTerminal(2)] },
                 Production { lhs: 1, rhs: vec![Letter::Terminal(EPSILON)] },
+            ],
+            nullable: None,
+        };
+
+        assert_eq!(grammar, result);
+    }
+
+    #[test]
+    fn test_remove_useless() {
+        let mut grammar = {
+            // S -> AB | a
+            // B -> b
+
+            // S = 0
+            // B = 1
+            // A = 2
+
+            Grammar {
+                start_symbol: 0,
+                productions: vec![
+                    Production { lhs: 0, rhs: vec![Letter::NonTerminal(1), Letter::NonTerminal(2)] },
+                    Production { lhs: 0, rhs: vec![Letter::Terminal('a')] },
+                    Production { lhs: 1, rhs: vec![Letter::Terminal('b')] },
+                ],
+                nullable: None,
+            }
+        };
+
+        grammar.remove_useless();
+
+        let result = Grammar {
+            start_symbol: 0,
+            productions: vec![
+                Production { lhs: 0, rhs: vec![Letter::Terminal('a')] },
             ],
             nullable: None,
         };
